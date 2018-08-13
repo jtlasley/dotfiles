c5da0507c2bd98bc89db4b31bb4b7633d623ce4c
mk <magiking@magiking.org>
2018-08-13 15:01:51 -0600

    Changes to groups and layout.

--- bin/bin/lemonbar/barfeed.sh
+++ bin/bin/lemonbar/barfeed.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
-#
+#!/bin/bash
+
 # Thanks to zebra for his awesome writeup on lemonbar
 # mk's new lemonbar feeder!
 
-bgcolor="#63a690" #cloudy
-fgcolor="#eeeeee"
+. grabcolors.sh
+bgcolor="#$blu"
+fgcolor="#$bg"
+
+# using wal
+#. "${HOME}/.cache/wal/colors.sh"
+#bgcolor="$color0"
+#fgcolor="$color8"
 
 clock(){
-    date '+%T %D'
+    date '+%A, %b%_d %r'
 }
 
 battery(){
@@ -29,8 +35,12 @@ volume(){
 network(){
     # for 3 interfaces: loopbak, ethernet, wifi
     read lo int1 int2 <<< `ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p'`
+    
 
-    # iwconfig returns an error code if the interface tseted has no wireless extensions
+
+    # iwconfig returns an error code if the interface tseted has no wireless 
+    # extensions
+
     if iwconfig $int1 >/dev/null 2>&1; then
         wifi=$int1
         eth0=$int2
@@ -43,60 +53,30 @@ network(){
 
     echo -n "$int "
 
-    ping -c 1 8.8.8.8 >/dev/null 2>&1 && echo "connected" || echo "disconnected"
+    ping -c 1 8.8.8.8 >/dev/null 2>&1 && echo "con" || echo "dis"
 }
 
 groups(){
     cur=`xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}'`
-    tot=`xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}'`
 
     case $cur in
         0) echo "%{F$bgcolor}━━━%{F-} ━━━ ━━━ ━━━ ━━━" ;;
-        1) echo "━━━ %{F$bgcolor}━━━%{F-} ━━━ ━━━ ━━━" ;;
-        2) echo "━━━ ━━━ %{F$bgcolor}━━━%{F-} ━━━ ━━━" ;;
-        3) echo "━━━ ━━━ ━━━ %{F$bgcolor}━━━%{F-} ━━━" ;;
-        4) echo "━━━ ━━━ ━━━ ━━━ %{F$bgcolor}━━━%{F-}" ;;
+        1) echo "%{F-}━━━ %{F$bgcolor}━━━%{F-} ━━━ ━━━ ━━━" ;;
+        2) echo "%{F-}━━━ ━━━ %{F$bgcolor}━━━%{F-} ━━━ ━━━" ;;
+        3) echo "%{F-}━━━ ━━━ ━━━ %{F$bgcolor}━━━%{F-} ━━━" ;;
+        4) echo "%{F-}━━━ ━━━ ━━━ ━━━ %{F$bgcolor}━━━%{F-}" ;;
     esac
-
-    #for w in `seq 0 $((cur - 1))`; do line="${line}="; done
-    #line="${line}|"
-    #for w in `seq $((cur + 2)) $tot`; do line="${line}="; done
-    #echo $line
 }
-
-workspaces () {
-    #col="#d23266"
-    #xprop -spy -root _NET_CURRENT_DESKTOP | \
-    xprop -spy -root _NET_CURRENT_DESKTOP | \
-    while read line; do
- 
-        num=$(echo $line | awk '{print $3}')
-   
-        echo -n "%{c}%{+o}"
-        case $num in
-            0) echo "%{F$bgcolor}━━━%{F-} ━━━ ━━━ ━━━ ━━━" ;;
-            1) echo "━━━ %{F$bgcolor}━━━%{F-} ━━━ ━━━ ━━━" ;;
-            2) echo "━━━ ━━━ %{F$bgcolor}━━━%{F-} ━━━ ━━━" ;;
-            3) echo "━━━ ━━━ ━━━ %{F$bgcolor}━━━%{F-} ━━━" ;;
-            4) echo "━━━ ━━━ ━━━ ━━━ %{F$bgcolor}━━━%{F-}" ;;
-        esac
-    done
-}
-
-
-#bgcolor="#ef5847" #onodera
-bgcolor="#63a690" #cloudy
-fgcolor="#eeeeee"
 
 # This loop will fill a bufer with info, then output it to stduot
 while :; do
     buf=""
     #buf="${buf} %{F$fgcolor}%{B$bgcolor}%{l}    [$(groups)] %{F-}%{B-}"
-    buf="${buf} %{F$fgcolor}%{B-}%{l}    $(groups) %{F-}%{B-}"
-    buf="${buf} %{F$fgcolor}%{B$bgcolor}%{c} NET: $(network) %{F-}%{B-}"
-    buf="${buf} %{F$fgcolor}%{B$bgcolor} VOL: $(volume) %{F-}%{B-}"
-    buf="${buf} %{F$fgcolor}%{B$bgcolor}%{r}| CLK:  $(clock) |%{F-}%{B-}"
-    buf="${buf} %{F$fgcolor}%{B$bgcolor}%{r} BAT:  $(battery) |%{F-}%{B-}"
+    buf="${buf} %{S-}%{F$fgcolor}%{B-}%{l}    $(groups) %{F-}%{B-}"
+    buf="${buf} %{F$fgcolor}%{B$bgcolor}%{c} $(clock) %{F-}%{B-}"
+    buf="${buf} %{F$fgcolor}%{B$bgcolor}%{r} VOL: $(volume) %{F-}%{B-}"
+    buf="${buf} %{F$fgcolor}%{B$bgcolor} NET: $(network) %{F-}%{B-}"
+    buf="${buf} %{F$fgcolor}%{B$bgcolor} BAT:  $(battery) %{F-}%{B-}"
 
     echo $buf
 
